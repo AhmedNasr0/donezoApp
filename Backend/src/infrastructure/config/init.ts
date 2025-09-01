edb69a89ea995982f8628a4398ad252b6aa574ce
Ahmed <ahmed@imagini.io>
2025-09-01 04:00:58 +0300

    Add process.env for deploy

--- Backend/src/infrastructure/config/init.ts
+++ Backend/src/infrastructure/config/init.ts
@@ -1,6 +1,6 @@
 export const databaseConfig = {
-    postgresUrl: 'postgresql://taskdb_k6cv_user:6dCazhg5QpbS70dLImDxI4p0DqS6wOa3@dpg-d2ohb324d50c739ue2o0-a.oregon-postgres.render.com/taskdb_k6cv',
-    redisUrl: 'rediss://red-d2ohqaggjchc73ep7oo0:91qTkwnGS1APbmiIWDjrRGEI35RA5rYk@oregon-keyvalue.render.com:6379'
+    postgresUrl: process.env.POSTGRES_CONNECTION_STRING || 'postgresql://taskdb_k6cv_user:6dCazhg5QpbS70dLImDxI4p0DqS6wOa3@dpg-d2ohb324d50c739ue2o0-a.oregon-postgres.render.com/taskdb_k6cv',
+    redisUrl: process.env.REDIS_CONNECTION_STRING ||'rediss://red-d2ohqaggjchc73ep7oo0:91qTkwnGS1APbmiIWDjrRGEI35RA5rYk@oregon-keyvalue.render.com:6379'
 }
 
 type appConfig ={
