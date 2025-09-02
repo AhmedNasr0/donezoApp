6256b332073d8bc7add2093fc4a69d15de9cd08f
Ahmed <ahmed@imagini.io>
2025-09-02 05:51:43 +0300

    Add env in the ai-worker

--- ai-worker/src/core/config.py
+++ ai-worker/src/core/config.py
@@ -1,10 +1,15 @@
 from pydantic_settings import BaseSettings
 from typing import Optional
+import os
+from dotenv import load_dotenv
+
+# حمل القيم من .env
+load_dotenv()
 
 class Settings(BaseSettings):
-    database_url: str = "postgresql://taskdb_k6cv_user:6dCazhg5QpbS70dLImDxI4p0DqS6wOa3@dpg-d2ohb324d50c739ue2o0-a.oregon-postgres.render.com/taskdb_k6cv"
+    database_url: str = os.getenv("DATABASE_URL") or "postgresql://taskdb_k6cv_user:6dCazhg5QpbS70dLImDxI4p0DqS6wOa3@dpg-d2ohb324d50c739ue2o0-a.oregon-postgres.render.com/taskdb_k6cv"
     
-    redis_url: str = "rediss://red-d2ohqaggjchc73ep7oo0:91qTkwnGS1APbmiIWDjrRGEI35RA5rYk@oregon-keyvalue.render.com:6379"
+    redis_url: str = os.getenv("REDIS_URL") or "rediss://red-d2ohqaggjchc73ep7oo0:91qTkwnGS1APbmiIWDjrRGEI35RA5rYk@oregon-keyvalue.render.com:6379"
     redis_pool_size: int = 20
     redis_max_connections: int = 50
     
